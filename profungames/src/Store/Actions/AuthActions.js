1bb451ea9e3f9a826dd5d5f335d51139e73be01b
mukesh4601 <bhardwaj.mukesh91@gmail.com>
2019-05-23 19:11:43 +0530

    API Done

---
+++ profungames/src/Store/Actions/AuthActions.js
@@ -0,0 +1,32 @@
+import { createAction } from "redux-actions";
+import axios from 'axios';
+import { BACKEND_URL } from "../../config.js";
+
+const VERIFY_USER = createAction("VERIFY_USER");
+
+export const verifyUser = values => dispatch => {
+    console.log(values);
+    return axios.post(
+        BACKEND_URL + 'api/token', {
+            username: values.username,
+            password: values.password,
+            RequestedAt: values.RequestedAt,
+            // headers: {
+            //     'Access-Control-Allow-Origin': '*'
+            // },
+            // crossdomain: true
+        }
+    ).then((res) => {
+        localStorage.setItem("token", res.response.data.token);
+        dispatch(VERIFY_USER(res.response))
+    }).catch(error => {
+        document.getElementById("emailerror").innerHTML = error.response.data.errorMessage;
+        console.log(error.response.data.errorMessage);
+        // return Promise.reject();
+    });
+}
+
+
+
+
+
--- profungames/src/index.js
+++ profungames/src/index.js
@@ -1,32 +1,52 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+
+
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { createStore, applyMiddleware, compose } from "redux";
+import Reducers from "./Store/Reducers/Index";
+
+
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 import { Route, BrowserRouter as Router } from "react-router-dom";
+
 import Login from "./components/login/login";
 import MobSearch from "./components/mobsearch/mobsearch";
 import NewEntry from "./components/newcustomer/newcustomer";
 import ExistingCus from "./components/existingcustomer/existingcustomer";
-// import Events from "./events/events";
-// import Packages from "./packages/packages";
 import Coupons from "./components/coupons/coupon";
-import EventSelection from "./components/eventselection/eventselection";
 import Summary from "./components/summary/summary";
 
+
+const composeEnhancers =
+    (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+            trace: true,
+            traceLimit: 25
+        })) ||
+    compose;
+
+const store = createStore(Reducers, composeEnhancers(applyMiddleware(thunk)));
+
+
+
 const routing = (
-    <Router>
-        <div>
-            <Route exact path="/" component={Login} />
-            <Route path="/mobsearch" component={MobSearch} />
-            <Route path="/newentry" component={NewEntry} />
-            <Route path="/existingcus" component={ExistingCus} />
-            {/* <Route path="/eventselection" component={EventSelection} /> */}
-            <Route path="/summary" component={Summary} />
-            <Route path="/coupon" component={Coupons} />
-        </div>
-    </Router>
+    <Provider store={store}>
+        <Router>
+            <div>
+                <Route exact path="/" component={Login} />
+                <Route path="/mobsearch" component={MobSearch} />
+                <Route path="/newentry" component={NewEntry} />
+                <Route path="/existingcus" component={ExistingCus} />
+                {/* <Route path="/eventselection" component={EventSelection} /> */}
+                <Route path="/summary" component={Summary} />
+                <Route path="/coupon" component={Coupons} />
+            </div>
+        </Router>
+    </Provider>
 );
 
 ReactDOM.render(routing, document.getElementById("root"));
