5ccd72e1eee33b8a2ba5617f8e431c0cf98c203a
mukesh4601 <bhardwaj.mukesh91@gmail.com>
2019-05-23 11:04:45 +0530

    Add

--- profungames/src/index.js
+++ profungames/src/index.js
@@ -4,10 +4,21 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+
+
+
+
+const routing = (
+    <Router>
+        <div>
+            <Route exact path="/" component={Home} />
+        </div>
+    </Router>
+)
+
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
